01d10dda165af65c46e5fd181ffdc09fbfad0264
Josh Patterson <josh@ntdls.com>
2024-02-22 17:32:13 -0500

    Randomized explosion particle fade - per particle.

--- Si.GameEngine/Core/TickControllers/ParticlesSpriteTickController.cs
+++ Si.GameEngine/Core/TickControllers/ParticlesSpriteTickController.cs
@@ -81,6 +81,7 @@ namespace Si.GameEngine.Core.TickControllers
                         var particle = CreateAt(at.X, at.Y, GraphicsUtility.GetRandomHotColor(), new Size(SiRandom.Between(1, 2), SiRandom.Between(1, 2)));
                         particle.Shape = ParticleShape.FilledEllipse;
                         particle.CleanupMode = ParticleCleanupMode.FadeToBlack;
+                        particle.FadeToBlackReductionAmount = SiRandom.Between(0.001, 0.01);
                         particle.Velocity.MaxSpeed *= SiRandom.Between(1, 3.5);
                         particle.VectorType = ParticleVectorType.Independent;
                     }
